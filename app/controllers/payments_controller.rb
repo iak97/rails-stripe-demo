3084f30afe5a73ebdc9b06c418690c3804c2e856
iak97 <psajithkumar97@gmail.com>
2023-11-26 19:28:10 +0530

    Added payment intent

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,5 +3,12 @@ class PaymentsController < ApplicationController
 
     def pay
         @order = Order.find(params[:order_id])
+
+        @payment_intent = Stripe::PaymentIntent.create({
+            amount: @order.total,
+            currency: 'usd',
+            automatic_payment_methods: {enabled: true},
+            description: "Order #{@order.id}"
+        })
     end
 end
